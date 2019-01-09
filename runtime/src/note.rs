2818bbbc9d56f67e6b4721640cf379f95028a8e2
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-01-09 19:00:25 +0900

    finish encrypt_note

--- runtime/src/note.rs
+++ runtime/src/note.rs
@@ -1,10 +1,17 @@
 extern crate substrate_primitives as primitives;
+extern crate parity_crypto as crypto;
+extern crate rust_crypto as rcrypto;
+
 use rand::OsRng;
 use primitives::{hashing::blake2_256, ed25519::{Pair, Public, PKCS_LEN}};
+use rcrypto::exchange;
+
+pub use crypto::KEY_ITERATIONS;
 
 pub type SecretKey = [u8; PKCS_LEN];
 // pub type Publickey = primitives::H256;
 pub type Publickey = Public;
+
 
 pub struct Note {
     pub value: u64,
@@ -12,7 +19,14 @@ pub struct Note {
     // pub E::Fs, // the commitment randomness
 }
 
-impl Note {
+pub struct EncryptedNote {
+    ciphertext: Vec<u8>,
+    iv: [u8; 16],
+    mac: [u8; 32],
+    ephemeral_public: Public, 
+}
+
+impl EncryptedNote {
     pub fn new(value: u64, public_key: Publickey) -> Result<Self, ECIESError> {
         Ok(Note {
             value,
@@ -21,26 +35,44 @@ impl Note {
     }
 
     /// Encrypt a Note with public key
-    pub fn encrypt_note(&self) -> Result<Vec<u8>, ECIESError> {
+    pub fn encrypt_note(&self, plain_note: &[u8; PKCS_LEN], public_key: Publickey) -> Result<Vec<u8>, ECIESError> {
         let mut rng = OsRng::new().expect("OS Randomness available on all supported platforms; qed");
         let data: &str = &self.value.to_string();
 
         let ephemeral_secret: [u8; PKCS_LEN] = rng.gen();
         let pair = Pair::from_seed(&ephemeral_secret);
         let ephemeral_public = pair.public();
+    
+        // let salt: [u8; 32] = rng.gen();
+        let shared_secret = exchange(&public_key.0, &ephemeral_secret);
+        
+		// [ DK[0..15] DK[16..31] ] = [derived_left_bits, derived_right_bits]
+        let (derived_left_bits, derived_right_bits) 
+            = crypto::derive_key_iterations(shared_secret, &public_key.0, KEY_ITERATIONS); // TODO: fix the type of public_key
 
         // an initialisation vector
         let iv: [u8; 16] = rng.gen();
-        let mut iv_encrypted = vec![0u8; 16 + data.len()];
-        iv_encrypted[0..16].copy_from_slice(iv.as_ref());
+        // let mut iv_encrypted = vec![0u8; 16 + data.len()];
+        // iv_encrypted[0..16].copy_from_slice(iv.as_ref());
 
-        let tag = 
+        let mut ciphertext = vec![0u8; PKCS_LEN];
+
+        cryto::aes::encrypt_128_ctr(&derived_left_bits, &iv, plain_note, &mut *ciphertext)
+            .expect("input lengths of key and iv are both 16; qed");
         
+        let mac = blake2_256(&crypto::derive_mac(&derived_right_bits, &*ciphertext));
 
-        let mut ret = vec![0u8; 65 + 16 + data.len() + 32];
+        EncryptedNote {
+            ciphertext,
+            iv,
+            mac,
+            ephemeral_public,
+        }
 
     }
 
     /// Decrypt a Note with secret key
-    pub fn decrypt_note(&self, secret_key: &SecretKey,)
+    pub fn decrypt_note(&self, secret_key: &SecretKey) {
+        let ephemeral_secret = exhange(&self.ephemeral_public.0, secret_key);
+    }
 }
