63b417f030aadafbcbe4d97a4b771d353984eee8
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-01-09 20:20:58 +0900

    finish implementing decrypt_note func

--- runtime/src/note.rs
+++ runtime/src/note.rs
@@ -12,6 +12,7 @@ pub type SecretKey = [u8; PKCS_LEN];
 // pub type Publickey = primitives::H256;
 pub type Publickey = Public;
 
+/// convert Vec<u8> to array[u8; 32]
 fn vec_as_u8_32_array(vector: Vec<u8>) -> [u8; 32] {
     let mut arr = [0u8; 32];
     for (place, element) in arr.iter_mut().zip(vector.iter()) {
@@ -20,6 +21,8 @@ fn vec_as_u8_32_array(vector: Vec<u8>) -> [u8; 32] {
     arr
 }
 
+// TODO: change to blake2_256
+/// Get keys for encrypt and mac through the key derivation function
 fn concat_kdf(key_material: [u8; 32]) -> ([u8; 16], [u8; 16]) {
     const SHA256BlockSize: usize = 64;
     const reps: usize = (32 + 7) * 8 / (SHA256BlockSize * 8);
@@ -59,8 +62,9 @@ impl EncryptedNote {
         })
     }
 
+    // TODO: fix type of plain_note 
     /// Encrypt a Note with public key
-    pub fn encrypt_note(&self, plain_note: &[u8; PKCS_LEN], public_key: Publickey) -> Result<Vec<u8>, ECIESError> {
+    pub fn encrypt_note(&self, plain_note: &[u8; PKCS_LEN], public_key: Publickey) -> Self {
         let mut rng = OsRng::new().expect("OS Randomness available on all supported platforms; qed");
         let data: &str = &self.value.to_string();
 
@@ -84,6 +88,7 @@ impl EncryptedNote {
         cryto::aes::encrypt_128_ctr(&derived_left_bits, &iv, plain_note, &mut *ciphertext)
             .expect("input lengths of key and iv are both 16; qed");
         
+        // Blake2_256(DK[16..31] ++ <ciphertext>), where DK[16..31] - derived_right_bits
         let mac = blake2_256(&crypto::derive_mac(&derived_right_bits, &*ciphertext));
 
         EncryptedNote {
@@ -96,7 +101,22 @@ impl EncryptedNote {
     }
 
     /// Decrypt a Note with secret key
-    pub fn decrypt_note(&self, secret_key: &SecretKey) {
-        let ephemeral_secret = exhange(&self.ephemeral_public.0, secret_key);
+    pub fn decrypt_note(&self, secret_key: &SecretKey) -> Result<[u8; PKCS_LEN]> {
+        let shared_secret = exhange(&self.ephemeral_public.0, secret_key);
+
+        // [ DK[0..15] DK[16..31] ] = [derived_left_bits, derived_right_bits]        
+        let (derived_left_bits, derived_right_bits) = concat_kdf(shared_secret); 
+
+        // Blake2_256(DK[16..31] ++ <ciphertext>), where DK[16..31] - derived_right_bits
+        let mac = blake2_256(&crypto::derive_mac(&derived_right_bits, &*ciphertext));
+
+        if subtle::slices_equal(&mac[..], &self.mac[..]) != 1 {
+			return Err(ErrorKind::InvalidPassword.into());
+		}
+
+        let mut plain = [0; PKCS_LEN];
+        crypto::aes::decrypt_128_ctr(&derived_left_bits, &self.iv, &self.ciphertext, &mut plain[..])
+            .expect("input lengths of key and iv are both 16; qed");
+        Ok(plain)
     }
 }
