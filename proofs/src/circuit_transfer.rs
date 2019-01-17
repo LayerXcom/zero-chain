7efbf38e269e72e4ef0d1d8339971df4738f698d
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-01-17 20:15:16 +0900

    add paymentaddress

--- proofs/src/circuit_transfer.rs
+++ proofs/src/circuit_transfer.rs
@@ -22,13 +22,14 @@ use sapling_crypt::{
 };
 
 pub struct Transfer<'a, E: JubjubEngine> {
-    pub params: &'a E::Params, // TODO
+    pub params: &'a E::Params, 
     pub sender_spending_key: Option<E::Fs>,
-    pub receiver_public_ley: Option<ProofGenerationKey<E>>,
-    old_value: Option<u64>, 
-    sender_value: Option<u64>,
-    receiver_value: Option<u64>,
-    ephmeral_secret_key: Option<E::Fs>,
+    // The payment address  of the recipient
+    pub payment_address: Option<PaymentAddress<E>>,
+    pub old_value: Option<u64>, 
+    pub sender_value: Option<u64>,
+    pub receiver_value: Option<u64>,
+    pub ephmeral_secret_key: Option<E::Fs>,
 }
 
 impl<'a, E: Engine> Circuit<E> for Transfer<'a, E> {
@@ -37,7 +38,23 @@ impl<'a, E: Engine> Circuit<E> for Transfer<'a, E> {
         cs: &mut CS
     ) -> Result<(), SynthesisError>
     {
+        let params = self.params;
 
+        // Prover witnesses g_d, ensuring it's on the curve.
+        let g_d = ecc::EdwardsPoint::witnesses(
+            cs.namespace(|| "witness g_d"),
+
+        )
+
+        // Create the ephemeral public key from g_d.
+        let epk = g_d.mul(
+            cs.namespace(|| "epk computation"),
+            &esk,
+            self.params
+        )?;
+
+        // Expose epk publicly.
+        epk.inputize(cs.namespace(|| "epk"))?;
     }
 }
 
