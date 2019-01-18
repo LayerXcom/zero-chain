142d8802256352df612784b1e049270dc48f5d59
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-01-18 09:59:01 +0900

    add circuits of the re-randomize ak

--- proofs/src/circuit_transfer.rs
+++ proofs/src/circuit_transfer.rs
@@ -23,13 +23,17 @@ use sapling_crypt::{
 
 pub struct Transfer<'a, E: JubjubEngine> {
     pub params: &'a E::Params, 
-    pub sender_spending_key: Option<E::Fs>,
+    pub proof_generation_key: Option<ProofGenerationKey<E>>,
+    // The payment address associated with the note
+    pub sender_payment_address: Option<PaymentAddress<E>>,
     // The payment address  of the recipient
-    pub payment_address: Option<PaymentAddress<E>>,
+    pub recipient_payment_address: Option<PaymentAddress<E>>,
     pub old_value: Option<u64>, 
     pub sender_value: Option<u64>,
     pub receiver_value: Option<u64>,
     pub ephmeral_secret_key: Option<E::Fs>,
+    // Re-randomization of the public key
+    pub ar: Option<E::Fs>,
 }
 
 impl<'a, E: Engine> Circuit<E> for Transfer<'a, E> {
@@ -43,7 +47,7 @@ impl<'a, E: Engine> Circuit<E> for Transfer<'a, E> {
         // Prover witnesses g_d, ensuring it's on the curve.
         let g_d = ecc::EdwardsPoint::witnesses(
             cs.namespace(|| "witness g_d"),
-            self.payment_address.as_ref().and_then(|a| a.g_d(params)),
+            self.recipient_payment_address.as_ref().and_then(|a| a.g_d(params)),
             self.params
         )?;
 
@@ -51,6 +55,11 @@ impl<'a, E: Engine> Circuit<E> for Transfer<'a, E> {
         g_d.assert_not_small_order(
             cs.namespace(|| "g_d not small order"),
             self.params
+        )?;
+        
+        let esk = boolean::field_into_boolean_vec_le(
+            cs.namespace(|| "esk"),
+            self.esk
         )?;
 
         // Create the ephemeral public key from g_d.
@@ -62,6 +71,43 @@ impl<'a, E: Engine> Circuit<E> for Transfer<'a, E> {
 
         // Expose epk publicly.
         epk.inputize(cs.namespace(|| "epk"))?;
+
+        // Ensure ak on the curve.
+        let ak = ecc::EdwardsPoint::witness(
+            cs.namespace(|| "ak"),
+            self.proof_generation_key.as_ref().map(|k| k.ak.clone()),
+            self.params
+        )?;
+
+        // Ensure ak is large order.
+        ak.assert_not_small_order(
+            cs.namespace(|| "ak not small order"),
+            self.params
+        )?;
+
+        // Re-randomize ak
+        {
+            let ar = boolean::field_into_boolean_vec_le(
+                cs.namespace(|| "ar"),
+                self.ar
+            )?;
+
+            let ar = ecc::fixed_base_multiplication(
+                cs.namespace(|| "computation of randomiation for the signing key"),
+                FixedGenerators::SpendingKeyGenerator,
+                &ar,
+                self.params
+            )?;
+
+            let rk = ak.add(
+                cs.namespace(|| "computation of rk"),
+                &ar,
+                self.params
+            )?;
+
+            rk.inputize(cs.namespace(|| "rk"))?;
+        }
+        // TODO: large order check for ak
     }
 }
 
