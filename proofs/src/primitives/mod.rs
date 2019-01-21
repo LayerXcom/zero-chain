2a7a5856d918f5d63b279cadef5619e79b1acfd4
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-01-21 13:24:22 +0900

    add ivk,, proof_generation_key, payment_address

--- proofs/src/primitives/mod.rs
+++ proofs/src/primitives/mod.rs
@@ -3,3 +3,81 @@ use pairing::{
     PrimeFieldRepr,
     Field,    
 };
+
+use scrypto::constants;
+
+use scrypto::jubjub::{
+    JubjubEngine,
+    JubjubParams,
+    edwards,
+    PrimeOrder,
+    FixedGenerators
+};
+
+#[derive(Clone)]
+pub struct ProofGenerationKey<E: JubjubEngine> {
+    pub ak: edwards::Point<E, PrimeOrder>,
+    pub nsk: E::Fs
+}
+
+impl<E: JubjubEngine> ProofGenerationKey<E> {
+    pub fn into_viewing_key(&self, params: &E::Params) -> ViewingKey<E> {
+        ViewingKey {
+            ak: self.ak.clone(),
+            nk: params.generator(FixedGenerators::ProofGenerationKey).mul(self.nsk, params)
+        }
+    }
+}
+
+pub struct ViewingKey<E: JubjubEngine> {
+    pub ak: edwards::Point<E, PrimeOrder>,
+    pub nk: edwards::Point<E, PrimeOrder>
+}
+
+impl<E: JubjubEngine> ViewingKey<E> {
+    pub fn rk(
+        &self,
+        ar: E::Fs,
+        params: &E::Params
+    ) -> edwards::Point<E, PrimeOrder> {
+        self.ak.add(
+            &params.generator(FixedGenerators::SpendingKeyGenerator).mul(ar, params),
+            params
+        )
+    }
+
+    pub fn ivk(&self) -> E::Fs {
+        let mut preimage = [0; 64];
+        self.ak.write(&mut preimage[0..32].unwrap());
+        self.nk.write(&mut preimage[32..64].unwrap());
+
+        let mut h = Blake2s::with_params(32, &[], &[], constants::CRH_IVK_PERSONALIZATION);
+        h.update(&preimage);
+        let mut h = h.finalize().as_ref().to_vec();
+
+        h[31] &= 0b0000_0111;
+        let mut e = <E::Fs as PrimeField>::Repr::default();
+        e.read_le(&h[..]).unwarap();
+        E::Fs::from_repr(e).expect("should be a vaild scalar")
+    }
+
+    pub fn into_payment_address(
+        &self,
+        diversifier: Diversifier,
+        params: &E::Params
+    ) -> Option<PaymentAddress<E>>
+    {
+
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct Diversifier(pub [u8; 11]);
+
+
+#[derive(Clone)]
+pub struct PaymentAddress<E: JubjubEngine> {
+    pub pk_d: edwards::Point<E, PrimeOrder>,
+    pub diversifier: Diversifier
+}
+
