19e8c1c8dedc0420668f14a45184d3ae1f773f49
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-02-07 21:03:42 +0900

    Add verifier for zkproof

--- proofs/src/verifier.rs
+++ proofs/src/verifier.rs
@@ -57,12 +57,62 @@ pub fn check_proof (
     auth_sig: RedjubjubSignature,
     params: &JubjubBls12,
 ) -> bool {
-    if is_small_order(&cv, params) {
+    // Check the points are not small order
+    if is_small_order(&cv_transfer, params) {
+        return false;
+    }
+    if is_small_order(&cv_balance, params) {
         return false;
     }
     if is_small_order(&rk.0, params) {
         return false;
     }
 
-}
+    // Compute the signature's message for rk/auth_sig
+    let mut data_to_be_signed = [0u8; 64];
+    rk.0.write(&mut data_to_be_signed[0..32])
+        .expect("message buffer should be 32 bytes");
+    (&mut data_to_be_signed[32..64]).copy_from_slice(&sighash_value[..]);
+
+    // Verify the auth_sig
+    if !rk.verify(
+        &data_to_be_signed,
+        &auth_sig,
+        FixedGenerators::SpendingKeyGenerator,
+        params,
+    ) {
+        return false;
+    }
+
+    // Construct public input for circuit
+    let mut public_input = [Fr::zero(), 8];
+    {
+            let (x, y) = (&cv_balance).cm(params).into_xy();
+            public_input[0] = x;
+            public_input[1] = y;
+        }
+        {
+            let (x, y) = (&cv_transfer).cm(params).into_xy();
+            public_input[2] = x;
+            public_input[3] = y;
+        }
+        {
+            let (x, y) = epk.into_xy();
+            public_input[4] = x;
+            public_input[5] = y;
+        }
+        {
+            let (x, y) = rk.0.into_xy();
+            public_input[6] = x;
+            public_input[7] = y;
+        }
+
+        // Verify the proof
+        match verify_proof(verifying_key, &zkproof, &public_input[..]) {
+            // No error, and proof verification successful
+            Ok(true) => true,
+            _ => false,                
+        }
+
+    }
 }
