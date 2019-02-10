7a0e231b0099b8798b7a6abfc7893a0ad716c9c9
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-02-10 13:30:38 +0900

    Add check_proof in module

--- runtime/src/confidential_transfer.rs
+++ runtime/src/confidential_transfer.rs
@@ -1,7 +1,11 @@
+//! A simple module for dealing with confidential transfer of fungible assets.
+
+// Ensure we're `no_std` when compiling for Wasm.
 // #![cfg_attr(not(feature = "std"), no_std)]
-use runtime_support::{StorageValue, StorageMap, dispatch::Result};
-use rstd::prelude::*;
-use {balances, system::ensure_signed};
+
+use runtime_support::{StorageValue, StorageMap, Parameter};
+use runtime_primitives::traits::{Member, SimpleArithmetic, Zero, StaticLookup};
+use system::ensure_signed;
 
 use bellman::groth16::{   
     PreparedVerifyingKey, 
@@ -31,48 +35,50 @@ use scrypto::{
 
 use proofs::{verifier::check_proof, primitives::PaymentAddress};
 
-pub trait Trait: balances::Trait {
-    // type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+pub trait Trait: system::Trait {
+	/// The units in which we record balances.
+	type Balance: Member + Parameter + SimpleArithmetic + Default + Copy;
 }
 
-decl_module! {
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // fn deposit_event() = default;
-        
-        fn confidential_transfer (
+type AssetId = u32;
+
+decl_module! {	
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {		
+		fn confidential_transfer(
             _origin,
-        //     // zkproof: Proof<Bls12>,
-        //     // cv_transfer: edwards::Point<Bls12, PrimeOrder>,
-        //     // cv_balance: edwards::Point<Bls12, PrimeOrder>,
-        //     // epk: edwards::Point<Bls12, PrimeOrder>,
-        //     // rk: PublicKey<Bls12>,
-        //     // sig_verifying_key: &PreparedVerifyingKey<Bls12>,
-        //     // sighash_value: &[u8; 32],
-        //     // auth_sig: RedjubjubSignature,
-        //     // params: &JubjubBls12,
-        )
-        {
-        //     // TODO: Elaborate Err message
-        //     // ensure!(check_proof(
-        //     //     zkproof,
-        //     //     cv_transfer,
-        //     //     cv_balance,
-        //     //     epk,
-        //     //     rk,
-        //     //     sig_verifying_key,
-        //     //     sighash_value,
-        //     //     auth_sig,
-        //     //     params
-        //     // ), "The zkproof is invalid.");
-            Ok(())
-        }
-    }
+            zkproof: Proof<Bls12>,
+            cv_transfer: edwards::Point<Bls12, PrimeOrder>,
+            cv_balance: edwards::Point<Bls12, PrimeOrder>,
+            epk: edwards::Point<Bls12, PrimeOrder>,
+            rk: PublicKey<Bls12>,
+            sig_verifying_key: PreparedVerifyingKey<Bls12>,
+            sighash_value: [u8; 32],
+            auth_sig: RedjubjubSignature,
+            params: JubjubBls12
+        ) {
+			// let origin = ensure_signed(origin)?;
+            ensure!(
+                check_proof(
+                    zkproof,
+                    cv_transfer,
+                    cv_balance,
+                    epk,
+                    rk,
+                    &sig_verifying_key,
+                    &sighash_value,
+                    auth_sig,
+                    &params
+                ),
+                "Invalid zkproof"
+            );
+            
+			
+		}		
+	}
 }
 
-impl<T: Trait> Module<T> {}
 
-// decl_storage! {
-//     trait Store for Module<T: Trait> as ConfidentialTransfer {
-//         pub CommittedBalance get(commited_balance): map PaymentAddress<Bls12> => edwards::Point<Bls12, PrimeOrder>;
-//     }
-// }
+
+impl<T: Trait> Module<T> {
+	
+}
