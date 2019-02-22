1441dceca486590ad00e690a1a4262304504f85a
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-02-22 13:17:55 +0900

    comment out the test of mimc

--- crypto/src/mimc.rs
+++ crypto/src/mimc.rs
@@ -48,41 +48,41 @@ use super::constants::{DEFAULT_MIMC_ROUND, DEFAULT_MIMC_SEED, MIMC_PERSONALIZATI
 /// This is an implementation of MiMC.
 /// See http://eprint.iacr.org/2016/492 for more 
 /// information about this construction.
-pub fn mimc<E: JubjubEngine>(
-    mut x: E::Fs,
-    k: E::Fs,
-    constants: Vec<E::Fs>
-) -> E::Fs
-{
-    assert_eq!(constants.len(), DEFAULT_MIMC_ROUND);
+// pub fn mimc<E: JubjubEngine>(
+//     mut x: E::Fs,
+//     k: E::Fs,
+//     constants: Vec<E::Fs>
+// ) -> E::Fs
+// {
+//     assert_eq!(constants.len(), DEFAULT_MIMC_ROUND);
 
-    for i in 0..DEFAULT_MIMC_ROUND {
-        let mut tmp1 = x;
-        tmp1.add_assign(&k);
-        tmp1.add_assign(&constants[i]);    
-        let tmp2 = tmp1;
-        tmp1.square();
-        tmp1.square();
-        tmp1.square();
+//     for i in 0..DEFAULT_MIMC_ROUND {
+//         let mut tmp1 = x;
+//         tmp1.add_assign(&k);
+//         tmp1.add_assign(&constants[i]);    
+//         let tmp2 = tmp1;
+//         tmp1.square();
+//         tmp1.square();
+//         tmp1.square();
         
-        tmp1.mul_assign(&tmp2);                        
-        x = tmp1;
-    }
-    x.add_assign(&k);
-    x
-}
+//         tmp1.mul_assign(&tmp2);                        
+//         x = tmp1;
+//     }
+//     x.add_assign(&k);
+//     x
+// }
 
-#[test]
-fn test_mimc() {
-    let mut rng = &mut thread_rng();
-    let constants: Vec<Fs> = (0..DEFAULT_MIMC_ROUND)
-        .map(|_| rng.gen())
-        .collect::<Vec<Fs>>();
+// #[test]
+// fn test_mimc() {
+//     let mut rng = &mut thread_rng();
+//     let constants: Vec<Fs> = (0..DEFAULT_MIMC_ROUND)
+//         .map(|_| rng.gen())
+//         .collect::<Vec<Fs>>();
 
-    let x: Fs = Fs::rand(&mut rng);
-    let k: Fs = Fs::rand(&mut rng);
+//     let x: Fs = Fs::rand(&mut rng);
+//     let k: Fs = Fs::rand(&mut rng);
 
-    let cipher_text: Fs = mimc::<Bls12>(x, k, constants.clone());
-    let plain_text: Fs = mimc::<Bls12>(cipher_text, k, constants);
-    assert_eq!(x, plain_text);
-}
+//     let cipher_text: Fs = mimc::<Bls12>(x, k, constants.clone());
+//     let plain_text: Fs = mimc::<Bls12>(cipher_text, k, constants);
+//     assert_eq!(x, plain_text);
+// }
