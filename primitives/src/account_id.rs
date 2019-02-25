1493fc9e43e83152760009a810624df909c8474b
osuke <arget-fee.free.dgm@hotmail.co.jp>
2019-02-25 13:15:22 +0900

    pass test of into_from_accountId

--- primitives/src/account_id.rs
+++ primitives/src/account_id.rs
@@ -75,9 +75,24 @@ impl Into<AccountId> for PaymentAddress<Bls12> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::{Rand, Rng, SeedableRng, XorShiftRng, OsRng};    
+    use pairing::bls12_381::Bls12;
+    use crate::keys::*;
 
     #[test]
     fn test_into_from() {
-        unimplemented!();
+        let rng = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+        let mut seed = [0u8; 32];
+        rng.fill_bytes(&mut seed[..]);
+
+        let ex_sk = ExpandedSpendingKey::<Bls12>::from_spending_key(&seed[..]);
+        let viewing_key = ViewingKey::<Bls12>::from_expanded_spending_key(&ex_sk, &JUBJUB);
+        let diversifier = Diversifier::new::<Bls12>(&JUBJUB).unwrap();
+        let addr1 = viewing_key.into_payment_address(diversifier, &JUBJUB).unwrap();
+
+        let account_id = AccountId::from_payment_address(&addr1);
+        println!("account_id: {:?}", account_id);
+        let addr2 = account_id.into_payment_address().unwrap();
+        assert!(addr1 == addr2);
     }
 }
