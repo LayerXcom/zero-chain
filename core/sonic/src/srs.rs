947878881772c1f0d1ae24b2ca49a79c07472b0e
Osuke Sudo <dish230@gmail.com>
2019-04-10 19:49:51 +1000

    Add SRS new fun()

--- core/sonic/src/srs.rs
+++ core/sonic/src/srs.rs
@@ -1,6 +1,6 @@
 use pairing::{Engine, Wnaf, CurveAffine};
 
-/// Defined in $4.3: Structured Reference String
+/// Defined in Section 4.3: Structured Reference String
 #[derive(Clone, Eq, PartialEq)]
 pub struct SRS<E: Engine> {
     pub d: usize,
@@ -34,15 +34,49 @@ pub struct SRS<E: Engine> {
 impl<E: Engine> SRS<E> {
     pub fn new(d: usize, x: E::Fr, alpha: E::Fr) -> Self {
         let mut g1 = Wnaf::new();
-        let mut g1 = g1.base(E:G1::one(), d * 4);
+        let mut g1 = g1.base(E::G1::one(), d * 4);
+        let mut g2 = Wnaf::new();
+        let mut g2 = g2.base(E::G2::one(), d * 4);
 
+        // Generate the Affine point Vec to get SRS elements.
         fn table<C: CurveAffine>(
             mut cur: C::Scalar,
             step: C::Scalar,
             num: usize,
             // W-ary Non-Adjacent Form
-            table:
-        )
+            table: &mut Wnaf<usize, &[C::Projective], &mut Vec<i64>>,
+        ) -> Vec<C> {
+            let mut v = vec![];
+            for _ in 0..num {
+                // Push x^i to v as a wnaf(the base is Projective representation)
+                v.push(table.scalar(cur.into_repr()));
+                cur.mul_assign(&step);
+            }
+            // Normalizes a slice of projective elements so that conversion to affine is cheap
+            C::Projective::batch_normalization(&mut v);
+            let v = v.into_repr().map(|e| e.into_affine()).collect();
+            v
+        }
+
+        // Get parameters to construct SRS
+        let x_inv = x.inverse().unwrap();
+        let mut x_alpha = x;
+        x_alpha.mul_assign(&alpha);
+
+        let mut inv_x_alpha = x_inv;
+        inv_x_alpha.mul_assign(&alpha);
+
+        SRS {
+            d: d,
+            g_pos_x: table(E::Fr::one(), x, d + 1, &mut g1),
+            g_neg_x: table(E::Fr::one(), x_inv, d + 1, &mut g1),
+            g_pos_x_alpha: table(E::Fr::one(), x_alpha, d, &mut g1),
+            g_neg_x_alpha: table(E::Fr::one(), inv_x_alpha, d, &mut g1),
+            h_pos_x: table(E::Fr::one(), x, d + 1, &mut g2),
+            h_neg_x: table(E::Fr::one(), x_inv, d + 1, &mut g2),
+            h_pos_x_alpha: table(E::Fr::one(), x_alpha, d + 1, &mut g2),
+            h_neg_x_alpha: table(E::Fr::one(), inv_x_alpha, d + 1, &mut g2),
+        }
 
     }
 }
