d897740a9f87d4dd8d86b23acf3f553b615c602c
Osuke Sudo <dish230@gmail.com>
2019-07-07 18:29:58 +0900

    Add functions to encrypted-assets

--- modules/encrypted-assets/src/lib.rs
+++ modules/encrypted-assets/src/lib.rs
@@ -3,10 +3,11 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use support::{decl_module, decl_storage, decl_event, StorageMap, dispatch::Result, ensure, Parameter};
+use support::{decl_module, decl_storage, decl_event, StorageMap, dispatch::Result, ensure, Parameter, StorageValue};
 use rstd::prelude::*;
 use parity_codec::Codec;
 use runtime_primitives::traits::{Member, SimpleArithmetic, Zero, One, StaticLookup, MaybeSerializeDebug};
+use system::ensure_signed;
 use zprimitives::{
     PkdAddress,
     Ciphertext,
@@ -16,8 +17,8 @@ use zprimitives::{
 
 /// The module configuration trait.
 pub trait Trait: system::Trait {
-    // /// The overarching event type.
-    // type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+    /// The overarching event type.
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 
     /// The units in which we record encrypted balances.
     type EncryptedBalance: ElgamalCiphertext + Parameter + Member + Default + MaybeSerializeDebug + Codec;
@@ -28,37 +29,67 @@ pub trait Trait: system::Trait {
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // fn deposit_event<T>() = default;
+        fn deposit_event<T>() = default;
 
-        fn issue(origin, total: T::EncryptedBalance) {
+        /// Issue a new class of encrypted fungible assets. There are, and will only ever be, `total`
+		/// such assets and they'll all belong to the `issuer` initially. It will have an
+		/// identifier `AssetId` instance: this will be specified in the `Issued` event.
+        fn issue(
+            origin,
+            issuer: PkdAddress,
+            total: T::EncryptedBalance
+        ) {
+            let origin = ensure_signed(origin)?;
+
+            // TODO: Verifying zk proof
 
             let id = Self::next_asset_id();
-            // <NextAssetId<T>>::mutate(|id| *id += One::one());
-            // <EncryptedBalances<T>>::insert((id, ))
+            <NextAssetId<T>>::mutate(|id| *id += One::one());
+            <EncryptedBalances<T>>::insert((id, issuer.clone()), total.clone());
+            <TotalSupply<T>>::insert(id, total.clone());
+
+            Self::deposit_event(RawEvent::Issued(id, issuer, total));
         }
 
-        fn transfer(origin,
+        /// Move some encrypted assets from one holder to another.
+        fn transfer(
+            origin,
             id: T::AssetId,
             target: PkdAddress,
             amount: T::EncryptedBalance
-            ) {
+        ) {
+            let origin = ensure_signed(origin)?;
+
 
         }
 
-        fn destroy(origin, id: T::AssetId) {
+        /// Destroy any encrypted assets of `id` owned by `owner`.
+        fn destroy(
+            origin,
+            owner: PkdAddress,
+            id: T::AssetId
+        ) {
+            let origin = ensure_signed(origin)?;
+
+            // TODO: Verifying zk proof
+
+            let balance = <EncryptedBalances<T>>::take((id, owner.clone()));
+
+            Self::deposit_event(RawEvent::Destroyed(id, owner, balance));
 
         }
 
     }
 }
 
-// decl_event!(
-//     pub enum Event<T>
-//     where
-//     {
-
-//     }
-// );
+decl_event!(
+    pub enum Event<T> where <T as Trait>::AssetId, <T as Trait>::EncryptedBalance {
+        /// Some encrypted assets were issued.
+        Issued(AssetId, PkdAddress, EncryptedBalance),
+        /// Some encrypted assets were destroyed.
+        Destroyed(AssetId, PkdAddress, EncryptedBalance),
+    }
+);
 
 decl_storage! {
     trait Store for Module<T: Trait> as EncryptedAssets {
@@ -66,4 +97,10 @@ decl_storage! {
         NextAssetId get(next_asset_id): T::AssetId;
         TotalSupply: map T::AssetId => T::EncryptedBalance;
     }
+}
+
+#[cfg(feature = "std")]
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
