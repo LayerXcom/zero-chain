b6c1bb12c74a9f239175f8d2f8996a23cfb73dd3
Osuke Sudo <dish230@gmail.com>
2019-08-05 11:20:45 +0900

    Fix test for ciphertext primitives

--- core/primitives/src/ciphertext.rs
+++ core/primitives/src/ciphertext.rs
@@ -67,78 +67,49 @@ impl AsBytesRef for Ciphertext {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rand::{Rng, SeedableRng, XorShiftRng, Rand};
-    use pairing::PrimeField;
-    use jubjub::curve::{FixedGenerators, JubjubBls12, fs::Fs, ToUniform, JubjubParams};
+    use rand::{Rng, SeedableRng, XorShiftRng};
+    use jubjub::curve::{FixedGenerators, JubjubBls12};
     use parity_codec::{Encode, Decode};
     use keys::EncryptionKey;
 
-    #[test]
-    fn test_ciphertext_into_from() {
-        let rng_sk = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
-        let mut sk = [0u8; 32];
-        rng_sk.fill_bytes(&mut sk[..]);
-        let sk_fs = Fs::to_uniform(elgamal::elgamal_extend(&sk).as_bytes()).into_repr();
-
+    fn gen_ciphertext() -> elgamal::Ciphertext::<Bls12> {
+        let rng_seed = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
         let rng_r = &mut XorShiftRng::from_seed([0xbc4f6d47, 0xd62f276d, 0xb963afd3, 0x54558639]);
-        let mut randomness = [0u8; 32];
-        rng_r.fill_bytes(&mut randomness[..]);
-        let r_fs = Fs::to_uniform(elgamal::elgamal_extend(&randomness).as_bytes());
 
         let params = &JubjubBls12::new();
         let p_g = FixedGenerators::Diversifier;
+        let randomness = rng_r.gen();
+        let seed: [u8; 32] = rng_seed.gen();
 
-        let public_key = EncryptionKey(params.generator(p_g).mul(sk_fs, params));
-        let value: u32 = 5 as u32;
+        let enc_key = EncryptionKey::from_seed(&seed[..], params).unwrap();
+        let amount: u32 = 5 as u32;
 
-        let ciphertext1 = elgamal::Ciphertext::encrypt(value, r_fs, &public_key, p_g, params);
+        elgamal::Ciphertext::encrypt(amount, randomness, &enc_key, p_g, params)
+    }
 
-        let ciphertext_b = Ciphertext::from_ciphertext(&ciphertext1);
-        let ciphertext2 = ciphertext_b.into_ciphertext().unwrap();
+    #[test]
+    fn test_ciphertext_into_from() {
+        let ciphertext_from = gen_ciphertext();
+        let ciphertext = Ciphertext::from_ciphertext(&ciphertext_from);
+        let ciphertext_into = ciphertext.into_ciphertext().unwrap();
 
-        assert!(ciphertext1 == ciphertext2);
+        assert!(ciphertext_from == ciphertext_into);
     }
 
     #[test]
     fn test_ciphertext_encode_decode() {
-        let rng_sk = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
-        let mut sk = [0u8; 32];
-        rng_sk.fill_bytes(&mut sk[..]);
-        let sk_fs = Fs::to_uniform(elgamal::elgamal_extend(&sk).as_bytes()).into_repr();
-
-        let rng_r = &mut XorShiftRng::from_seed([0xbc4f6d47, 0xd62f276d, 0xb963afd3, 0x54558639]);
-        let mut randomness = [0u8; 32];
-        rng_r.fill_bytes(&mut randomness[..]);
-        let r_fs = Fs::to_uniform(elgamal::elgamal_extend(&randomness).as_bytes());
-
-        let params = &JubjubBls12::new();
-        let p_g = FixedGenerators::Diversifier;
-
-        let public_key = EncryptionKey(params.generator(p_g).mul(sk_fs, params));
-        let value: u32 = 5 as u32;
-
-        let ciphertext1 = elgamal::Ciphertext::encrypt(value, r_fs, &public_key, p_g, params);
-        let ciphertext_b = Ciphertext::from_ciphertext(&ciphertext1);
+        let ciphertext = gen_ciphertext();
+        let ciphertext_b = Ciphertext::from_ciphertext(&ciphertext);
 
         let encoded_cipher = ciphertext_b.encode();
-
         let decoded_cipher = Ciphertext::decode(&mut encoded_cipher.as_slice()).unwrap();
+
         assert_eq!(ciphertext_b, decoded_cipher);
     }
 
     #[test]
     fn test_ciphertext_rw() {
-        let params = &JubjubBls12::new();
-        let p_g = FixedGenerators::Diversifier;
-
-        let rng = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
-        let sk_fs = Fs::rand(rng);
-        let r_fs = Fs::rand(rng);
-
-        let public_key = EncryptionKey(params.generator(p_g).mul(sk_fs, params));
-        let value = 10 as u32;
-
-        let ciphertext = elgamal::Ciphertext::encrypt(value, r_fs, &public_key, p_g, params);
+        let ciphertext = gen_ciphertext();
 
         let mut buf = [0u8; 64];
         ciphertext.write(&mut &mut buf[..]).unwrap();
@@ -149,4 +120,3 @@ mod tests {
         assert_eq!(ciphertext_a, ciphertext_b);
     }
 }
-
