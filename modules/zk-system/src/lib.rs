0667e7e834e73b89fa64e8c92cbff4dd20bc5aed
Osuke Sudo <dish230@gmail.com>
2019-08-15 12:04:42 +0900

    Remove unnecessary imports

--- modules/zk-system/src/lib.rs
+++ modules/zk-system/src/lib.rs
@@ -1,26 +1,21 @@
 //! A module for dealing with zk-system
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use support::{decl_module, decl_storage, decl_event, StorageValue, StorageMap, dispatch::Result, Parameter};
+use support::{decl_module, decl_storage, StorageValue};
 use rstd::prelude::*;
 use rstd::result;
-use rstd::convert::{TryFrom, TryInto};
+use rstd::convert::TryFrom;
 use bellman_verifier::verify_proof;
 use pairing::{
     bls12_381::{Bls12,Fr},
     Field,
 };
-use runtime_primitives::traits::{Member, Zero, MaybeSerializeDebug, As};
+use runtime_primitives::traits::As;
 use jubjub::redjubjub::PublicKey;
 use jubjub::curve::{edwards, PrimeOrder};
-use zprimitives::{
-    EncKey, Proof, PreparedVk, ElgamalCiphertext,
-    SigVk, Nonce, GEpoch,
-};
-use parity_codec::Codec;
+use zprimitives::{PreparedVk, Nonce, GEpoch};
 use keys::EncryptionKey;
 use zcrypto::elgamal;
-use system::{IsDeadAccount, ensure_signed};
 
 pub trait Trait: system::Trait {
 	// The overarching event type.
